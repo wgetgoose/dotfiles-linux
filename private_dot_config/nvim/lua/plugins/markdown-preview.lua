2d4260eed36a5b234229957188f5702427775f3f
wgetgoose <carson@carsonferreira.com>
2024-02-13 23:04:25 -0800

    add markdown preview to nvim

---
+++ private_dot_config/nvim/lua/plugins/markdown-preview.lua
@@ -0,0 +1,13 @@
+return {
+  {
+    "iamcco/markdown-preview.nvim",
+    cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
+    -- below is yarn by default. using npm
+    --build = "cd app && yarn install",
+    build = "cd app && npm install",
+    init = function()
+      vim.g.mkdp_filetypes = { "markdown" }
+    end,
+    ft = { "markdown" },
+  },
+}
