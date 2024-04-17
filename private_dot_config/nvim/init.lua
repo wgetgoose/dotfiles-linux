d0581e40549c5ec4e0fe1bffcff13f95abfbef8b
wgetgoose <carson@carsonferreira.com>
2024-04-17 14:28:24 -0700

    back to lazyvim

--- private_dot_config/nvim/init.lua
+++ private_dot_config/nvim/init.lua
@@ -1,23 +1,2 @@
-local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
-if not vim.loop.fs_stat(lazypath) then
-  vim.fn.system({
-    "git",
-    "clone",
-    "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable", -- latest stable release
-    lazypath,
-  })
-end
-vim.opt.rtp:prepend(lazypath)
-require("lazy").setup("plugins")
-
--- set vim options with local variable
-local opt = vim.opt
--- Change default tab spacing 
-opt.shiftwidth = 2
--- Relative Line Number
-opt.relativenumber = true
--- Use system clipboard
-opt.clipboard = "unnamedplus"
-
+-- bootstrap lazy.nvim, LazyVim and your plugins
+require("config.lazy")
